cd2d8673607890bd729c142e11f2e219ef15baf5
marcel <marcel.chukwuma00@gmail.com>
2026-02-16 10:50:14 +0100

    cleanup

--- packages/server/startup/db.ts
+++ packages/server/startup/db.ts
@@ -4,7 +4,7 @@ import logger from '../services/logger.service';
 async function configureDB() {
    try {
       await mongoose.connect(process.env.DB_URL!);
-      logger.info(`Connected to DB: ${process.env.DB_URL}`);
+      logger.info(`Connected to DB: ${process.env.DB_URL}...`);
    } catch (error) {
       logger.error(error);
    }
