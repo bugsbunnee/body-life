edc113a5fd713f2b21ca6043d7cd6c59f15290fc
marcel <marcel.chukwuma00@gmail.com>
2026-03-27 10:18:15 +0100

    fix

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Toaster } from './components/ui/sonner';
 
